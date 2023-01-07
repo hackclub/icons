ee2bbe040b77d4172c6119fc5c4feec86ccbda37
Konv Suu <2583695112@qq.com>
2023-01-08 00:37:48 +0800

    feat: copy icon name to cllipboard

--- docs/pages/index.js
+++ docs/pages/index.js
@@ -1,12 +1,27 @@
 import React from "react";
 import Head from "next/head";
 import Icon, { glyphNames } from "@hackclub/icons";
+import copy from "copy-to-clipboard";
 
 const title = "@hackclub/icons";
 const description =
   "Hack Club’s iconset as React components, published on npm as @hackclub/icons.";
 
 const hidden = ["sus", "android"];
+
+let timer = null;
+
+function copyToClipboard(str) {
+  const copyEl = document.querySelector(".copy");
+  copy(str);
+  copyEl.classList.add("copied");
+  if(!timer) {
+    timer = setTimeout(() => copyEl.classList.remove("copied"), 1000);
+  }else {
+    clearTimeout(timer);
+    timer = setTimeout(() => copyEl.classList.remove("copied"), 1000);
+  }
+}
 
 const Docs = () => (
   <main>
@@ -47,6 +62,10 @@ const Docs = () => (
         font-size: 16px;
         font-weight: 600;
         margin: 0 16px;
+        transition: 0.125s ease-in-out;
+      }
+      nav a:hover {
+        color: #ec3750;
       }
       article {
         margin-top: 48px;
@@ -55,12 +74,58 @@ const Docs = () => (
         grid-gap: 16px;
         justify-content: center;
       }
+      article > div:not(:last-child) { 
+        cursor: pointer;
+      }
+      article > div:not(:last-child) > svg {
+        transition: fill 0.125s ease-in-out;
+      }
+      article > div:not(:last-child) > p{
+        transition: color 0.125s ease-in-out;
+      }
+      article > div:hover > svg {
+        fill: #ec3750;
+      }
+      article > div:hover > p{
+        color: #ec3750;
+      }
       p {
         color: #8b909a;
         line-height: 1.25;
         font-family: SFMono-Regular, Menlo, monospace;
         word-break: break-word;
         word-wrap: wrap;
+      }
+      .copy {
+        position: fixed;
+        z-index: 1;
+        top: 50%;
+        left: 50%;
+        trnasform: translate(-50%, -50%);
+
+        display: flex;
+        align-items: center;
+        gap: 8px;
+        padding: 0 24px;
+        border-radius: 5px;
+        border: 1px solid #ec3750;
+        background-color: #1e1e1e;
+
+        transition: opacity 0.125s ease-in-out;
+        opacity: 0;
+        visibility: hidden;
+      }
+      .copy.copied {
+        opacity: 1;
+        visibility: visible;
+      }
+      .copy > p {
+        color: #ec3750;
+        font-size: 16px;
+        font-weight: 600;
+      }
+      .copy > svg {
+        fill: #ec3750;
       }
       @media (prefers-color-scheme: dark) {
         body {
@@ -83,12 +148,16 @@ const Docs = () => (
       {glyphNames.sort().map(
         (key) =>
           !hidden.includes(key) && (
-            <div key={key}>
+            <div key={key} onClick={() => copyToClipboard(key)}>
               <Icon glyph={key} title={key} size={48} />
               <p children={key} />
             </div>
           )
       )}
+      <div className="copy">
+        <Icon glyph="copy-check" title="copy-check" size={24} /> 
+        <p>Copied</p>
+      </div>
     </article>
   </main>
 );
