7a7f5bd9d1257102f324e1cdac581ea1c8faeb34
Echo <github@3kh0.net>
2026-03-06 22:17:56 -0500

    fix vercel builds part 2

--- docs/next.config.js
+++ docs/next.config.js
@@ -1,9 +1,4 @@
-const path = require("path");
-
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  turbopack: {
-    root: path.join(__dirname, ".."),
-  },
 };
