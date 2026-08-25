3d9724c11b258e887b21f6c05b56f4bed769ed41
Echo <github@3kh0.net>
2026-08-24 21:38:19 -0400

    next for docker

--- docs/next.config.js
+++ docs/next.config.js
@@ -1,4 +1,8 @@
+const path = require("path");
+
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  output: "standalone",
+  outputFileTracingRoot: path.join(__dirname, ".."),
 };
